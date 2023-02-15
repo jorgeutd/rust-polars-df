9a99bef1a31b2aeea18111a8950bb20dc9b51436
Jorge Grisman <jorge.grisman@quantum-health.com>
2023-02-15 16:40:37 -0500

    adding data folder and titanic csv examples in the projects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use polars::prelude::{CsvReader, DataFrame, PolarsError};
 use std::result::Result;
 use polars::prelude::SerReader;
@@ -10,14 +11,32 @@ fn load_csv_file(file_path: &str) -> Result<DataFrame, PolarsError> {
     Ok(df)
 }
 
+
+fn count_passengers_by_sex(df: DataFrame) -> DataFrame {
+    df.groupby(&vec!["Sex"])
+        .expect("Failed to group by 'Sex' column")
+        .select(&["PassengerId"])
+        .count()
+        .unwrap()
+}
+
+
+
+
+
 fn main() {
     let file_path = "data/test.csv";
     let df = load_csv_file(file_path).unwrap();
-    let comments_col = df.column("Name").unwrap();
-    let comments_col_head = comments_col.head(Some(5));
-    println!("{}", df.head(Some(5)));
-    println!("{}", comments_col_head);
+    let name_col = df.column("Name").unwrap();
+    let name_col_head = name_col.head(Some(5));
+    let count_df = count_passengers_by_sex(df.clone());
     println!("{:?}", df.shape());
+    println!("{:?}",  df.get_column_names());
+    println!("{}", df.head(Some(5)));
+    println!("{}", name_col_head);
+
+    println!("{}", count_df);
+    
 }
 
 /* references 
